b3781fee7ab6aeac1bed752202c8d3c981373baf
RafaAudibert <rafaeelaudibert@gmail.com>
2020-04-23 00:08:50 -0300

    :sparkles: Added logic for snippets/dashboard page

--- src/pages/snippets.js
+++ src/pages/snippets.js
@@ -1,7 +1,37 @@
+import { useState, useContext, useEffect } from 'react'
+
 import PageWrapper from '../components/Common/PageWrapper'
 
-export default () => (
+import { BackendContext } from '../contexts/BackendContext'
+import { ErrorContext } from '../contexts/ErrorContext'
+
+
+const Snippets = () => {
+  const [ snippets, setSnippets ] = useState( null )
+
+  const { setUnauthorizedError } = useContext( ErrorContext )
+  const { user } = useContext( BackendContext )
+
+  useEffect( () => {
+    if ( user ) {
+      setSnippets( user.snippets.data )
+    }
+  }, [ user ] )
+
+  // Can only access the snippets page if it is authenticated
+  if ( !user ) {
+    setUnauthorizedError()
+  }
+
+  return (
+    'Snippets page body'
+  )
+}
+
+const Page = () => (
   <PageWrapper>
-    Snippets page body
+    <Snippets/>
   </PageWrapper>
 )
+
+export default Page
