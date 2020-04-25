435be2b68a6e8764d47c5ffa105dc959eff1677f
RafaAudibert <rafaeelaudibert@gmail.com>
2020-04-25 01:49:17 -0300

    :bug: Fix bug with ReponsiveContext

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Grommet, ResponsiveContext } from 'grommet'
+import { Grommet } from 'grommet'
 import Loader from './components/Common/Loader'
 import Theme from './theme'
 
@@ -40,9 +40,7 @@ const OuterWrapper = ( { children } ) => (
   <ErrorProvider>
     <BackendProvider>
       <Grommet theme={Theme} full>
-        <ResponsiveContext.Provider>
-          {children}
-        </ResponsiveContext.Provider>
+        {children}
       </Grommet>
     </BackendProvider>
   </ErrorProvider>
