29b59c36bca9b79c26b07ded761d673a6d7cef37
RafaAudibert <rafaeelaudibert@gmail.com>
2020-04-27 02:28:08 -0300

    :arrow_up::sparkles: Added Clipboard Copy component

---
+++ src/components/Common/ClipboardButton.js
@@ -0,0 +1,37 @@
+import { useClipboard } from 'use-clipboard-copy'
+import { Button } from 'grommet'
+
+export default ( {
+  text='',
+  buttonText='Copy to clipboard',
+  notSupportedButtonText='Copy is not supported',
+  copiedButtonText='Copied!',
+  copiedTimeout=600, // eslint-disable-line no-magic-numbers
+  ...props
+} ) => {
+  const clipboard = useClipboard( {
+    copiedTimeout
+  } )
+
+  if ( !!clipboard.isSupported() ) {
+    return <Button
+      primary
+      disabled={true}
+      size='small'
+      label={notSupportedButtonText}
+      color='neutral-2'
+      {...props}
+    />
+  }
+
+  return (
+    <Button
+      primary
+      color='neutral-1'
+      size='small'
+      label={clipboard.copied ? copiedButtonText : buttonText}
+      onClick={() => clipboard.copy( text )}
+      {...props}
+    />
+  )
+}
